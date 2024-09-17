d4ff8638e11c5843d698b356b78b085c7d475ade
tjelinek <tomasjelinek96@gmail.com>
2024-09-17 16:24:14 +0200

    FlowTracker-style config

--- cfg.py
+++ cfg.py
@@ -1,55 +1,61 @@
 from pathlib import Path
 import numpy as np
+from dataclasses import dataclass, field
+from typing import Dict, Set
 
-LOG_DIR = './logs/fit/'
+@dataclass
+class Config:
+    log_dir: Path = Path('./logs/fit/')
 
-format_to_dtype = {
-    'uchar': np.uint8,
-    'char': np.int8,
-    'ushort': np.uint16,
-    'short': np.int16,
-    'uint': np.uint32,
-    'int': np.int32,
-    'float': np.float32,
-    'double': np.float64,
-    'complex': np.complex64,
-    'dpcomplex': np.complex128,
-}
+    format_to_dtype: Dict[str, np.dtype] = field(default_factory=lambda: {
+        'uchar': np.uint8,
+        'char': np.int8,
+        'ushort': np.uint16,
+        'short': np.int16,
+        'uint': np.uint32,
+        'int': np.int32,
+        'float': np.float32,
+        'double': np.float64,
+        'complex': np.complex64,
+        'dpcomplex': np.complex128,
+    })
 
-dtype_to_format = {
-    'uint8': 'uchar',
-    'int8': 'char',
-    'uint16': 'ushort',
-    'int16': 'short',
-    'uint32': 'uint',
-    'int32': 'int',
-    'float32': 'float',
-    'float64': 'double',
-    'complex64': 'complex',
-    'complex128': 'dpcomplex',
-}
+    dtype_to_format: Dict[str, str] = field(default_factory=lambda: {
+        'uint8': 'uchar',
+        'int8': 'char',
+        'uint16': 'ushort',
+        'int16': 'short',
+        'uint32': 'uint',
+        'int32': 'int',
+        'float32': 'float',
+        'float64': 'double',
+        'complex64': 'complex',
+        'complex128': 'dpcomplex',
+    })
 
-mapping_classes_feit = {'adenocarcinoma': 'adenocarcinoma',
-                        'artefact': 'unknown',
-                        'blood': 'blood_and_vessels',
-                        'blood_vessel': 'blood_and_vessels',
-                        'connective_tissue': 'connective_tissue',
-                        'fat': 'fat',
-                        'inflammation_purulent': 'inflammation_purulent',
-                        'ink': 'unknown',
-                        'muscle_cross_section': 'muscle_cross_section',
-                        'muscle_longitudinal_section': 'muscle_longitudinal_section',
-                        'necrosis': 'necrosis',
-                        'nerve': 'nerve',
-                        'normal_mucosa': 'normal_mucosa',
-                        'empty': 'empty',
-                        'fragmucosa': 'unknown'}
+    mapping_classes_feit: Dict[str, str] = field(default_factory=lambda: {
+        'adenocarcinoma': 'adenocarcinoma',
+        'artefact': 'unknown',
+        'blood': 'blood_and_vessels',
+        'blood_vessel': 'blood_and_vessels',
+        'connective_tissue': 'connective_tissue',
+        'fat': 'fat',
+        'inflammation_purulent': 'inflammation_purulent',
+        'ink': 'unknown',
+        'muscle_cross_section': 'muscle_cross_section',
+        'muscle_longitudinal_section': 'muscle_longitudinal_section',
+        'necrosis': 'necrosis',
+        'nerve': 'nerve',
+        'normal_mucosa': 'normal_mucosa',
+        'empty': 'empty',
+        'fragmucosa': 'unknown'
+    })
 
-selected_classes_feit = {'adenocarcinoma', 'blood_and_vessels', 'connective_tissue', 'fat', 'inflammation_purulent',
-                         'muscle_cross_section', 'muscle_longitudinal_section', 'necrosis', 'nerve', 'normal_mucosa',
-                         'empty',
-                         # unknown
-                         }
+    selected_classes_feit: Set[str] = field(default_factory=lambda: {
+        'adenocarcinoma', 'blood_and_vessels', 'connective_tissue', 'fat', 'inflammation_purulent',
+        'muscle_cross_section', 'muscle_longitudinal_section', 'necrosis', 'nerve', 'normal_mucosa', 'empty'
+    })
 
-selected_classes_kather = {'01_TUMOR', '02_STROMA', '03_COMPLEX', '04_LYMPHO', '05_DEBRIS', '06_MUCOSA', '07_ADIPOSE',
-                           '08_EMPTY'}
+    selected_classes_kather: Set[str] = field(default_factory=lambda: {
+        '01_TUMOR', '02_STROMA', '03_COMPLEX', '04_LYMPHO', '05_DEBRIS', '06_MUCOSA', '07_ADIPOSE', '08_EMPTY'
+    })
